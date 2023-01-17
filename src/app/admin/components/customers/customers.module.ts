cee32e51aab1974b0e5735c40497fda5fd222e31
baran <baran@LAPTOP-F0E73CQV>
2023-01-17 19:25:06 +0300

    new project

---
+++ src/app/admin/components/customers/customers.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path:"",component:CustomerComponent}
+    ])
+  ]
+})
+export class CustomersModule { }
